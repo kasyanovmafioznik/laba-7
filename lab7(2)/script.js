9815e21a5f2cd4b0a9f904abf0195fd819d3c8b4
kasyanovmafioznik <77916322+kasyanovmafioznik@users.noreply.github.com>
2022-12-09 03:03:09 +0300

    Add files via upload

---
+++ lab7(2)/script.js
@@ -0,0 +1,43 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+    
+    button.on('click', (e) => {
+      e.preventDefault();
+      toggleMenu();
+    });
+    
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+    
+    function toggleMenu(){
+      menu.toggleClass('burger-menu_active');
+      
+      if (menu.hasClass('burger-menu_active')) {
+        $('body').css('overlow', 'hidden');
+      } else {
+        $('body').css('overlow', 'visible');
+      }
+    }
+  }
+  
+  burgerMenu('.burger-menu');
